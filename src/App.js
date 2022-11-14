9c8f0ab0acfd08ae57e83f9308a6bc9aa94c7d09
DevShasa <shasa.thuo@gmail.com>
2022-11-14 21:41:11 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import FormInput from "./components/FormInput"; 
+import { useState } from "react";
+import { inputItems } from "./utils/inputs";
 
 function App() {
+
+  const [ values, setValues ] = useState({
+      username:"",
+      email:"",
+      birthday:"",
+      password:"",
+      confirmPassword:""
+  })
+
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    // const data = new FormData(e.target)
+    // console.log(Object.fromEntries(data.entries()))
+
+    console.log(values)
+  }
+
+  const onChange = (e) =>{
+    setValues({...values, [e.target.name]:e.target.value})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={handleSubmit}>
+          {inputItems.map((input)=>(
+            <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
+          ))}
+          <button type="submit">
+              Submit
+          </button>
+        </form> 
     </div>
   );
 }
